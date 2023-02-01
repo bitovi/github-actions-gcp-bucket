1beb2fd01e45e7bbc5c31b160294bb6f5c9964c5
Max Cascone <max@bitovi.com>
2023-02-01 15:15:22 -0600

    generate url

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,11 +1,7 @@
-output "bucket_name" {
-  value = google_storage_bucket.default.name
-}
-
 output "bucket_id" {
   value = google_storage_bucket.default.id
 }
 
 output "file_url" {
-  value = google_storage_bucket_object.default.media_link
+  value = "${var.bucket_domain}/${google_storage_bucket.default.id}/${var.file_name}"
 }
