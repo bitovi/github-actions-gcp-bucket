190b13b11128d6a2f01fbf89777d767aa2b95cd7
Max Cascone <max@bitovi.com>
2023-02-01 15:19:04 -0600

    empty file variables, default domain

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,12 +1,12 @@
 # uses env var TF_VAR_GCP_PROJECT_ID
 variable "GCP_PROJECT_ID" {}
 
-variable "file_name" {
-  required = true
-  default  = ""
-}
+# name and source require setting either in a local .tfvars file,
+# or programatically
+variable "file_name" {}
 
-variable "file_source" {
-  required = true
-  default  = ""
+variable "file_source" {}
+
+variable "bucket_domain" {
+  default = "https://storage.googleapis.com"
 }
