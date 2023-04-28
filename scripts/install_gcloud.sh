342d396bb5d057adf5bfd8c215624034d63bdac6
mcascone <max@bitovi.com>
2023-04-27 21:41:05 -0500

    install gc take 2

--- scripts/install_gcloud.sh
+++ scripts/install_gcloud.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-snap install google-cloud-cli --classic
-gcloud init
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-428.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-428.0.0-linux-x86.tar.gz
+./google-cloud-sdk/install.sh --quiet
+./google-cloud-sdk/bin/gcloud init
