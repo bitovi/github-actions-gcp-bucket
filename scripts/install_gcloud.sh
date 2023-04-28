c66454ee7c5fd05d6cfb32ee3acb1b6ec59d417f
mcascone <max@bitovi.com>
2023-04-27 21:43:41 -0500

    typo in google's docs

--- scripts/install_gcloud.sh
+++ scripts/install_gcloud.sh
@@ -2,6 +2,6 @@
 
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-428.0.0-linux-x86_64.tar.gz
 ls
-tar -xf google-cloud-cli-428.0.0-linux-x86.tar.gz
+tar -xf google-cloud-cli-428.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --quiet
 ./google-cloud-sdk/bin/gcloud init
