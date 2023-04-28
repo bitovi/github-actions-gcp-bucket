8d8b2c54fa10cd0d1017522e1292f05b26e3451c
mcascone <max@bitovi.com>
2023-04-27 21:54:45 -0500

    add skip-diags, WIP

--- scripts/install_gcloud.sh
+++ scripts/install_gcloud.sh
@@ -4,4 +4,4 @@ curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-
 ls
 tar -xf google-cloud-cli-428.0.0-linux-x86_64.tar.gz
 ./google-cloud-sdk/install.sh --quiet
-./google-cloud-sdk/bin/gcloud init
+./google-cloud-sdk/bin/gcloud init --skip-diagnostics
